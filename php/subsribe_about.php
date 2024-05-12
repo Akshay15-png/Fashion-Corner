5edf6df0f432561a5310a14dbd84b166c0ab59c4
kaali <93660973+Akshay15-png@users.noreply.github.com>
2024-05-12 21:02:43 +0530

    Updated

--- php/subsribe_about.php
+++ php/subsribe_about.php
@@ -19,7 +19,7 @@ $result = $con->query($checkQuery);
 if ($result->num_rows > 0) {
     // not inserting email
     echo "<script>alert('Subscribed successfully');</script>";
-    echo "<script>window.location.href = '../index.html';</script>"; 
+    echo "<script>window.location.href = '../about.html';</script>"; 
     exit();
 }
 
@@ -29,11 +29,11 @@ $insertQuery = "INSERT INTO `subscribers` (`Email`, `Date/Time`) VALUES ('$userE
 if ($con->query($insertQuery) === TRUE) {
     sleep(1);
     echo "<script>alert('Subscribed successfully');</script>";
-    echo "<script>window.location.href = '../index.html';</script>"; 
+    echo "<script>window.location.href = '../about.html';</script>"; 
     exit();
 } else {
     echo"<script>alert('Backend Problem')</script>";
-    echo "<script>window.location.href = '../index.html';</script>"; 
+    echo "<script>window.location.href = '../about.html';</script>"; 
     exit();
 }
 
